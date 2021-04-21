8f2cbc79dadc0cc3ac51807ab14ddb737026bd03
Antoine Lelong <azenoxe@gmail.com>
2021-04-21 23:40:53 +0200

    change travis config for gh-pages

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import routes from 'virtual:generated-pages';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/'),
   routes
 });
 
