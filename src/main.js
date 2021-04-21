b5fa5358d517c745b4d48c9dd7c74a6f3ad9628b
Antoine Lelong <azenoxe@gmail.com>
2021-04-21 20:52:32 +0200

    added gh-pages to deploy and fix some issues with it

--- src/main.js
+++ src/main.js
@@ -9,8 +9,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import routes from 'virtual:generated-pages';
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+  history: createWebHistory('/'),
+  routes
 });
 
 createApp(App).use(router).mount('#app')
--- src/pages/projects/notion/index.vue
+++ src/pages/projects/notion/index.vue
@@ -1,3 +1,13 @@
 <template>
   <Board />
 </template>
+
+<script setup>
+
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+console.log(router.getRoutes());
+
+</script>
